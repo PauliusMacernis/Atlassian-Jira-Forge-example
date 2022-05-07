d8629b6e4b62cd1e1c31d7c7dc95f40905e6e29f
Paulius Macernis <paulius.macernis@interactio.io>
2022-05-07 10:56:59 +0300

    Output the number of comments to Jira UI: https://developer.atlassian.com/platform/forge/change-the-jira-frontend-with-the-ui-kit/

--- src/index.jsx
+++ src/index.jsx
@@ -18,7 +18,9 @@ const App = () => {
 
     return (
         <Fragment>
-            <Text>Hello world!</Text>
+            <Text>
+                Number of comments on this issue: {comments.length}
+            </Text>
         </Fragment>
     );
 };
